2909c991da9bed6fd9d0185251ee47bd457b06f7
dipu123-gupta <kumardipu845420@gmail.com>
2025-12-14 07:39:14 +0530

    express install and create multiple routes

--- Backend_Day5/index.js
+++ Backend_Day5/index.js
@@ -1,19 +1,36 @@
-const express = require('express');
-const app = express();
+const http = require("http");
 
-app.use('/', (req, res) => {
-    res.send({name: "dipu", age: 20, gender: "male"})
-})
 
-app.use('/about', (req, res) => {
-    res.send("this is my about page")
-})
+// ! Create server 
+// const server = http.createServer((req, res) => {
+//   res.write("Hello, this is my first node server ");
+//   res.end();
+// });
 
-app.use('/contact', (req, res) => {
-    res.send("this is my contact page")
-})
+// ! Example
+// const server = http.createServer((req, res) => {
+//   console.log(req.url); // Requested URL
+//   res.writeHead(200, { "Content-Type": "text/html" });
+//   res.write("<h1>Welcome to My Node.js Server</h1>");
+//   res.write("<h1>Hello welcom to my second server</h1>")
+//   res.end();
+// });
 
-app.listen(3000, () => {
-    console.log("Server running at port number 3000");
+// ! basic routing 
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>This is home page</h1>");
+  }else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>This is about page</h1>");
+  } else{
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>404 page is not found </h1>");
+  }
+  res.end();
+});
 
-})
+server.listen(3000, () => {
+  console.log("server is running on port 3000.. ");
+});
