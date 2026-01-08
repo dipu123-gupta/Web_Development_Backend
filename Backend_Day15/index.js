7a3fd0a85e1e2c9a72ba38aa014e729404985096
dipu123-gupta <kumardipu845420@gmail.com>
2026-01-08 21:20:49 +0530

    optimize code

---
+++ Backend_Day15/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+// const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db.js");
+const userRoutes = require("./routes/userRoute.js");
+const client=require("./config/redis.js")
+
+const app = express();
+const PORT=process.env.PORT
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend Running ✅");
+});
+
+const initializeConnection=async()=>{
+  try {
+    await Promise.all([connectDB(),client.connect()]);
+    console.log("db connected");
+    
+
+    app.listen(process.env.PORT, () => {
+      console.log("Server Running at port number:" + process.env.PORT);
+    });
+
+  } catch (error) {
+    console.log("Error"+error);
+    
+  }
+}
+
+initializeConnection();
+
+
+ 
